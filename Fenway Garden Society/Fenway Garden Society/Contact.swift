700a4060a211f15d5e21aea6465f76e60cc354e3
luminator123 <jasonlu6@bu.edu>
2017-02-06 20:03:03 -0500

    Adds text box for the gardener
    Add information from Professor Bruno Rubio.

---
+++ Fenway Garden Society/Fenway Garden Society/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  Fenway Garden Society
+//
+//  Created by Jason Lu on 1/30/17.
+//  Copyright © 2017 BU Global App Initiative. All rights reserved.
+//
+
+import Foundation
+
+// class ScrollView: UIScrollView {
+    
+// }
