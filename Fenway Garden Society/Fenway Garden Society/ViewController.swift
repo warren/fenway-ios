dbe0aaf8547339dd1bff85ee4c0361f2c3e25246
Warren-Partridge <maps@perdix.org>
2017-03-25 15:55:56 -0400

    Adds working version of AppAuth
    And it turns out we don’t even need it, because the FGS calendar was
    public all along. Wow.
    
    Life is about the journey, not the destination, right?

--- Fenway Garden Society/Fenway Garden Society/ViewController.swift
+++ Fenway Garden Society/Fenway Garden Society/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 import GoogleAPIClient
 // import GTMOAuth2 THIS IS DEPRECATED ACCORDING TO A GOOGLE POST ON GITHUB
 import GTMAppAuth
+import AppAuth
+import GoogleAPIClient
+import GoogleAPIClientForREST
 
 class ViewController: UIViewController {
     
     private let kKeychainItemName = "iOS client 1"
     private let kClientID = "301673697222-a2i8to3ld0qihr4gc9cu6t4pjht3ke3a.apps.googleusercontent.com"
+    private let kRedirectURI = "com.fenwaygardens.app:oauth_login_redirect"
+    private let kIssuer = "https://accounts.google.com"
     
     // If modifying these scopes, delete your previously saved credentials by
     // resetting the iOS simulator or uninstall the app.
@@ -23,30 +28,29 @@ class ViewController: UIViewController {
     private let service = GTLServiceCalendar()
     let output = UITextView()
     
+    var authState: OIDAuthState?
+    @IBOutlet weak var fileListView: UIPickerView!
+    
+
+    let appDelegate = (UIApplication.shared.delegate! as! AppDelegate)
+    var authorization: GTMAppAuthFetcherAuthorization?
+    var kExampleAuthorizerKey = "authorization"
+    let userDefaults = UserDefaults.standard
+    
+    
     // When the view loads, create necessary subviews
     // and initialize the Google Calendar API service
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        output.frame = view.bounds
-        output.isEditable = false
-        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
-        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        
-        view.addSubview(output);
-        
-        if let auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychain(
-            forName: kKeychainItemName,
-            clientID: kClientID,
-            clientSecret: nil) {
-                service.authorizer = auth
-            }
+        auth() // Utilize dark forbidden API witchcraft
     }
     
-    // When the view appears, ensure that the Google Calendar API service is authorized
+    
+    // CALENDAR When the view appears, ensure that the Google Calendar API service is authorized
     // and perform API calls
-    override func viewDidAppear(_ animated: Bool) {
+    /*override func viewDidAppear(_ animated: Bool) {
         if let authorizer = service.authorizer,
             let canAuth = authorizer.canAuthorize, canAuth {
                 fetchEvents()
@@ -57,10 +61,82 @@ class ViewController: UIViewController {
                 completion: nil
             )
         }
+    }*/
+    
+    
+    @IBAction func authorize(_ sender: Any)
+    {
+        auth()
     }
     
-    // Construct a query and get a list of upcoming events from the user calendar
-    func fetchEvents() {
+    
+    func saveAuth()
+    {
+        if authorization != nil && (authorization?.canAuthorize())! {
+            let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject: authorization!)
+            userDefaults.set(encodedData, forKey: kExampleAuthorizerKey)
+            userDefaults.synchronize()
+        }
+        else {
+            userDefaults.removeObject(forKey: kExampleAuthorizerKey)
+        }
+    }
+    
+    
+    func auth()
+    {
+        //need this potatoe
+        let issuer = URL(string: kIssuer)!
+        let redirectURI = URL(string: kRedirectURI)!
+        
+        //logMessage("Fetching configuration for issuer: " + issuer.description)
+        // discovers endpoints
+        OIDAuthorizationService.discoverConfiguration(forIssuer: issuer, completion: {(_ configuration: OIDServiceConfiguration?, _ error: Error?) -> Void in
+            if configuration == nil {
+                //self.logMessage("Error retrieving discovery document: " + (error?.localizedDescription)!)
+                return
+            }
+            //self.logMessage("Got configuration: " + configuration!.description)
+            // builds authentication request
+            let scopes = ["https://www.googleapis.com/auth/calendar.readonly"/*, kGTLRAuthScopeDrive*/]
+            let request = OIDAuthorizationRequest(configuration: configuration!, clientId: self.kClientID, scopes: scopes, redirectURL: redirectURI, responseType: OIDResponseTypeCode, additionalParameters: nil)
+            // performs authentication request
+    
+            
+            //self.logMessage("Initiating authorization request with scope: " + request.scope!.description)
+            
+            OIDAuthState.authState(byPresenting: request, presenting: self, callback: {(_ authState: OIDAuthState?, _ error: Error?) -> Void in
+                if authState != nil {
+                    self.authorization = GTMAppAuthFetcherAuthorization(authState: authState!)
+                    self.service.authorizer = self.authorization
+                    
+                    //self.saveAuth()
+                    print("successful log in")
+                }
+            })
+        })
+    }
+    
+    
+    func loadAuth()
+    {
+        if let _ = userDefaults.object(forKey: kExampleAuthorizerKey)
+        {
+            let decoded = userDefaults.object(forKey: kExampleAuthorizerKey) as! Data
+            let testAuth = NSKeyedUnarchiver.unarchiveObject(with: decoded) as! GTMAppAuthFetcherAuthorization
+            authorization = testAuth
+            service.authorizer = authorization
+        }
+    }
+
+    // aW5mb0BmZW53YXl2aWN0b3J5Z2FyZGVucy5vcmc
+    
+    // https://www.googleapis.com/calendar/v3/calendars/info%40fenwayvictorygardens.org/events?key=AIzaSyByz6zzKDLD966LSS2tPucaQPwfezihMA8
+    
+    
+    
+    // CALENDAR Construct a query and get a list of upcoming events from the user calendar
+    /*func fetchEvents() {
         let query = GTLQueryCalendar.queryForEventsList(withCalendarId: "info@fenwayvictorygardens.org")
         query?.maxResults = 10
         query?.timeMin = GTLDateTime(date: NSDate() as Date!, timeZone: NSTimeZone.local)
@@ -71,10 +147,10 @@ class ViewController: UIViewController {
             delegate: self,
             didFinish: "displayResultWithTicket:finishedWithObject:error:"
         )
-    }
+    }*/
     
-    // Display the start dates and event summaries in the UITextView
-    func displayResultWithTicket(
+    // CALENDAR Display the start dates and event summaries in the UITextView
+    /*func displayResultWithTicket(
         ticket: GTLServiceTicket,
         finishedWithObject response : GTLCalendarEvents,
         error : NSError?) {
@@ -101,10 +177,10 @@ class ViewController: UIViewController {
         }
         
         output.text = eventString
-    }
+    }*/
     
-    // Creates the auth controller for authorizing access to Google Calendar API
-    private func createAuthController() -> GTMOAuth2ViewControllerTouch {
+    // CALENDAR Creates the auth controller for authorizing access to Google Calendar API
+    /*private func createAuthController() -> GTMOAuth2ViewControllerTouch {
         let scopeString = scopes.joined(separator: " ")
         return GTMOAuth2ViewControllerTouch(
             scope: scopeString,
@@ -114,11 +190,11 @@ class ViewController: UIViewController {
             delegate: self,
             finishedSelector: "viewController:finishedWithAuth:error:"
         )
-    }
+    }*/
     
-    // Handle completion of the authorization process, and update the Google Calendar API
+    // CALENDAR Handle completion of the authorization process, and update the Google Calendar API
     // with the new credentials.
-    func viewController(vc : UIViewController,
+    /*func viewController(vc : UIViewController,
                         finishedWithAuth authResult : GTMOAuth2Authentication, error : NSError?) {
         
         if let error = error {
@@ -145,7 +221,7 @@ class ViewController: UIViewController {
         )
         alert.addAction(ok)
         present(alert, animated: true, completion: nil)
-    }
+    }*/
     
 
     override func didReceiveMemoryWarning() {
--- Fenway Garden Society/Pods/Target Support Files/Pods-Fenway Garden Society/Pods-Fenway Garden Society-frameworks.sh
+++ Fenway Garden Society/Pods/Target Support Files/Pods-Fenway Garden Society/Pods-Fenway Garden Society-frameworks.sh
@@ -90,6 +90,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/GTMOAuth2/GTMOAuth2.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleAPIClient/GoogleAPIClient.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleAPIClientForREST/GoogleAPIClientForREST.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SideMenu/SideMenu.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -99,5 +100,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/GTMOAuth2/GTMOAuth2.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleAPIClient/GoogleAPIClient.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleAPIClientForREST/GoogleAPIClientForREST.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SideMenu/SideMenu.framework"
 fi
