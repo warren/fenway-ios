88c89e1d95cfe2354046a438380a73dd6b2da576
Natalie Pienkowska <natalie.pienkowska@gmail.com>
2017-03-25 19:39:46 -0400

    Map
    Finally finished the clickable map :)

---
+++ Fenway Garden Society/Fenway Garden Society/MapController.swift
@@ -0,0 +1,34 @@
+//
+//  MapController.swift
+//  Fenway Garden Society
+//
+//  Created by Natalie Pienkowska on 3/25/17.
+//  Copyright © 2017 BU Global App Initiative. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+    
+    @IBOutlet weak var map: UIImageView!
+    
+    @IBAction func AccessibleGarden(_ sender: Any) {
+    }
+    @IBAction func WheelbarrowYard(_ sender: Any) {
+    }
+
+    @IBAction func FlagpoleInfo(_ sender: Any) {
+    }
+    @IBAction func Reeds(_ sender: Any) {
+    }
+    @IBAction func FlagpoleBoard(_ sender: Any) {
+    }
+    @IBAction func CommunityNursery(_ sender: Any) {
+    }
+    @IBAction func HerbGarden(_ sender: Any) {
+    }
+    @IBAction func TeachingGarden(_ sender: Any) {
+    }
+    @IBAction func TeachingApiary(_ sender: Any) {
+    }
+}
