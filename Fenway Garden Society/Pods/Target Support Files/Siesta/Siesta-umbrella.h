44ba1adcf8e3b90b38ad8ece622e38a53df32b61
Warren-Partridge <maps@perdix.org>
2017-03-30 17:32:15 -0400

    Installs pod dependencies for calendar
    pod 'GoogleAPIClientForREST', '~> 1.2.1'
    pod 'GTMAppAuth', '~> 0.5.0'
    pod 'Siesta', '~> 1.1.0'
    pod 'SwiftyJSON', '~> 3.1.4'
    pod 'Alamofire', '~> 4.4'

---
+++ Fenway Garden Society/Pods/Target Support Files/Siesta/Siesta-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "Siesta.h"
+#import "SiestaUI.h"
+
+FOUNDATION_EXPORT double SiestaVersionNumber;
+FOUNDATION_EXPORT const unsigned char SiestaVersionString[];
+
